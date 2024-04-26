4af45b131bcad0fb0b29117f5b3b00f0b6f079f9
Jorge Aita <aitajorge@yahoo.com.ar>
2024-04-26 00:02:44 -0300

    conversor funcionando

--- src/principal/CotizaWeb.java
+++ src/principal/CotizaWeb.java
@@ -1,7 +1,35 @@
+// En CotizaWeb.java
 package principal;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-public record CotizaWeb(String base_code,
-                        JsonObject conversion_rates) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public record CotizaWeb(String base_code, JsonObject conversion_rates) {
+    public static CotizaWeb buscaCotizacion(String monedaCotizacion) {
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/bd677ae8782ac9e96e1fa928/latest/" + monedaCotizacion);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            JsonObject jsonResponse = new Gson().fromJson(response.body(), JsonObject.class);
+            String baseCode = jsonResponse.get("base_code").getAsString();
+            JsonObject conversionRates = jsonResponse.getAsJsonObject("conversion_rates");
+            return new CotizaWeb(baseCode, conversionRates);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error en la solicitud HTTP");
+        }
+    }
 }
+
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,10 +1,76 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Principal {
-    public static void main(String[] args){
-        CotizaWeb  consulta = new CotizaWeb();
-        System.out.println("ARS");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CotizaWeb consulta = CotizaWeb.buscaCotizacion("USD");
 
+        if (consulta != null) {
+            while (true) {
+                System.out.println("*********************************************************");
+                System.out.println("Sea bienvenido al Conversor de Moneda   =]");
 
+                System.out.println("1. Dólar =>> Peso argentino");
+                System.out.println("2. Peso argentino =>> Dólar");
+                System.out.println("3. Dólar =>> Real brasileño");
+                System.out.println("4. Real brasileño =>> Dólar");
+                System.out.println("5. Dólar =>> Peso colombiano");
+                System.out.println("6. Peso colombiano =>> Dólar");
+
+                System.out.println("7. Salir");
+
+                System.out.println("\nElija una opción válida: ");
+                System.out.println("*********************************************************");
+                int opcion = scanner.nextInt();
+
+                if (opcion == 7) {
+                    System.out.println("¡Hasta luego!");
+                    break;
+                }
+
+                double tasaBase = 1.0;
+                String monedaDestino = "";
+
+                switch (opcion) {
+                    case 1:
+                    case 2:
+                        monedaDestino = "ARS";
+                        break;
+                    case 3:
+                    case 4:
+                        monedaDestino = "BRL";
+                        break;
+                    case 5:
+                    case 6:
+                        monedaDestino = "COP";
+                        break;
+                    default:
+                        System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+                        continue;
+                }
+
+                tasaBase = consulta.conversion_rates().get(monedaDestino).getAsDouble();
+
+                if (opcion == 1 || opcion == 3 || opcion == 5) {
+                    System.out.println("Ingrese la cantidad en USD a convertir:");
+                    double cantidadUSD = scanner.nextDouble();
+                    double cantidadDestino = cantidadUSD * tasaBase;
+                    System.out.println(cantidadUSD + " USD equivalen a " + cantidadDestino + " " + monedaDestino);
+                } else {
+                    System.out.println("Ingrese la cantidad en la moneda destino a convertir a USD:");
+                    double cantidadDestino = scanner.nextDouble();
+                    double cantidadUSD = cantidadDestino / tasaBase;
+                    System.out.println(cantidadDestino + " " + monedaDestino + " equivalen a " + cantidadUSD + " USD");
+                }
+            }
+        } else {
+            System.out.println("No se pudo obtener la cotización para la moneda base especificada.");
+        }
+
+        scanner.close();
     }
 }
+
+
